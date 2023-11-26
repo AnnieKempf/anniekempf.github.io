ce66294f97c0995d03487329a4e76857255a3ac1
Annie Kempf <annie.kempf@gmail.com>
2023-11-26 13:27:30 +0100

    Practice changes

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import LanguageSwitcher from './components/LanguageSwitcher'; // Assuming you ha
 
 function App() {
   return (
-    <div className="App">
+    <div id="home" className="App">
       <nav className="main-nav">
         <div className="nav-container">
-          <a className="nav-brand" href="#home">Annie</a>
+          <a className="nav-brand" href="#home">Annie Kempf</a>
           <div className="nav-collapse">
             <ul className="nav-list">
-              <li className="nav-item">
-                <a className="nav-link" href="#projects">Skills</a>
+            <li className="nav-item">
+                <a className="nav-link" href="#home">HOME</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#skills">Projects</a>
+                <a className="nav-link" href="#about">ABOUT</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#contact">Contact</a>
+                <a className="nav-link" href="#projects">PROJECTS</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#contact">CONTACT</a>
               </li>
             </ul>
             <LanguageSwitcher />
@@ -26,15 +29,15 @@ function App() {
       </nav>
 
       <header className="app-header">
-        <h1>Hello, I'm Annie</h1>
-        <p>Aspiring Full-Stack Developer with a passion for technology and design.</p>
-        <a href="https://www.youtube.com/" target="_blank" className="resume-link">Link to Youtube</a>
+        <h1 className="header-title">Hello, I'm Annie</h1>
+        <p className ="header-text">Aspiring Full-Stack Developer with a passion for technology and design.</p>
         <button className="download-button">Download resume</button>
       </header>
 
-      <section id="projects" className="section">
-        <h2>Projects</h2>
-        {/* Your project content */}
+      <section id="about" className="section">
+        <h2>About</h2>
+        <p className="paragraph-text1" >Paragraph text 1</p>
+        <p className="paragraph-text2">hej</p>
       </section>
 
       <section id="skills" className="section light-bg">
