1f6a3d2fc981a8d091f5d6639ef1393d05b5fe1b
Annie Kempf <annie.kempf@gmail.com>
2023-11-28 22:06:51 +0100

    Organized css, added styling to sections

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import LanguageSwitcher from './components/LanguageSwitcher'; // Assuming you ha
 function App() {
   return (
     <div id="home" className="App">
+
+
       <nav className="main-nav">
         <div className="nav-container">
           <a className="nav-brand" href="#home">Annie Kempf</a>
@@ -30,14 +32,18 @@ function App() {
 
       <header className="app-header">
         <h1 className="header-title">Hello, I'm Annie</h1>
-        <p className ="header-text">Aspiring Full-Stack Developer with a passion for technology and design.</p>
+        <p className ="header-text">Aspiring <span className="developer-text">Full-Stack</span> Developer with a passion for technology and design.</p>
         <button className="download-button">Download resume</button>
       </header>
 
       <section id="about" className="section">
-        <h2>About</h2>
-        <p className="paragraph-text1" >Paragraph text 1</p>
-        <p className="paragraph-text2">hej</p>
+        <h2 className="about-title">Who am I?</h2>
+        <h2 className="skills-title">Explore my skills</h2>
+        <p className="about-text1">Well, <strong>I'm Annie</strong> - an Information System Science student graduating in May 2024, aiming to excel as a full-stack developer.</p>
+        <p className="about-text2">My enthusiasm for technology is driven by a 
+          love for problem-solving and creating efficient, user-centric solutions. With a competitive spirit inspired by strategic gaming, I'm eager to tackle challenges and 
+          contribute to a dynamic team environment. I'm committed to continuous learning and applying my knowledge, bringing both determination and creativity to overcome any 
+          obstacle in the tech world.</p>
       </section>
 
       <section id="skills" className="section light-bg">
@@ -49,6 +55,8 @@ function App() {
         <h2>Contact</h2>
         {/* Your contact content */}
       </section>
+
+
     </div>
   );
 }
