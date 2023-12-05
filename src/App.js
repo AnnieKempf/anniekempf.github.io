5d6675826586c9b037c7e0774f4d975d1ae586df
Annie Kempf <annie.kempf@gmail.com>
2023-12-05 19:03:54 +0100

    Minor changes + Created grid on About page

--- src/App.js
+++ src/App.js
@@ -31,22 +31,57 @@ function App() {
       </nav>
 
       <header className="app-header">
-        <h1 className="header-title">Hello, I'm Annie</h1>
-        <p className="header-text">Aspiring <span className="developer-text">Full-Stack Developer</span> with a passion for logic and problem-solving.</p>
+        <h1 className="header-title">
+          Hello, I'm Annie
+        </h1>
+        <p className="header-text">
+          Aspiring <span className="developer-text">Full-Stack Developer</span> with a passion for logic and problem-solving.
+        </p>
         <button className="download-button">Download resume</button>
       </header>
 
       <section id="about" className="section">
         <div className="about-container">
           <h2 className="about-title">Who am I?</h2>
-          <p className="about-text1">Well, <strong>I'm Annie</strong> - an Information System Science student graduating in May 2024, aiming to excel as a full-stack developer.</p>
-          <p className="about-text2">My enthusiasm for technology is driven by a
+          <p className="about-text1">
+            Well, <strong>I'm Annie</strong> - an Information System Science student graduating in May 2024, aiming to excel as a full-stack developer.
+          </p>
+          <p className="about-text2">
+            My enthusiasm for technology is driven by a
             love for problem-solving and creating efficient, user-centric solutions. With a competitive spirit inspired by strategic gaming, I'm eager to tackle challenges and
             contribute to a dynamic team environment. I'm committed to continuous learning and applying my knowledge, bringing both determination and creativity to overcome any
-            obstacle in the tech world.</p>
+            obstacle in the tech world.
+          </p>
+          <img className="picture" src="profilepic.jpeg" alt="Profil picture" width="350" height="400"></img>
         </div>
+
         <div className="skills-container">
           <h2 className="skills-title">Skills</h2>
+          <div className="skills-category">
+            <p className="skills-category-titles">
+              Front-End
+            </p>
+            <p className="skills-category-titles">
+              Back-End
+            </p>
+            <p className="skills-category-titles">
+              Other tools
+            </p>
+          </div>
+          <div className="skills-grid">
+            <p>React</p>
+            <p>HTML</p>
+            <p>CSS</p>
+            <p>Java</p>
+            <p>C#</p>
+            <p>SQL</p>
+            <p>Node.js</p>
+            <p>Azure</p>
+            <p>.NET</p>
+            <p>Javascript</p>
+            <p>Git</p>
+            <p>Github</p>
+          </div>
         </div>
       </section>
 
